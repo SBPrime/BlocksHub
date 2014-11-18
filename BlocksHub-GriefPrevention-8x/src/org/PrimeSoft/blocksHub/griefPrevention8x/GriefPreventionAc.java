32a50640d90ee526eae2a99f9a84e82046e42ec6
SBPrime <SB_Prime@poczta.onet.pl>
2014-11-18 16:02:26 +0100

    BlocksHub FIX: FIxed initialization FIX: Fixed namespace

--- BlocksHub-GriefPrevention-8x/src/org/PrimeSoft/blocksHub/griefPrevention8x/GriefPreventionAc.java
+++ BlocksHub-GriefPrevention-8x/src/org/PrimeSoft/blocksHub/griefPrevention8x/GriefPreventionAc.java
@@ -39,7 +39,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package org.PrimeSoft.blocksHub;
+package org.PrimeSoft.blocksHub.griefPrevention8x;
 
 import me.ryanhamshire.GriefPrevention.BlockEventHandler;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
