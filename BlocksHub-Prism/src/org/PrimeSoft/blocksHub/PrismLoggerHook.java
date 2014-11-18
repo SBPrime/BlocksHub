2c7754917a45dfd1964f584453964d714aba1830
SBPrime <SB_Prime@poczta.onet.pl>
2014-11-18 15:27:05 +0100

    BlocksHub CHG: Changed project property

--- BlocksHub-Prism/src/org/PrimeSoft/blocksHub/PrismLoggerHook.java
+++ BlocksHub-Prism/src/org/PrimeSoft/blocksHub/PrismLoggerHook.java
@@ -40,7 +40,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.PrimeSoft.blocksHub.blocklogger;
+package org.PrimeSoft.blocksHub;
 
 import java.lang.reflect.Method;
 
