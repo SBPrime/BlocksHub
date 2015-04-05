8eeda11bda04619c472091e70ad9f89e766329c5
SB_Prime <sb_prime@poczta.onet.pl>
2015-04-05 16:23:51 +0200

    BlocksHub-GriefPrevention CHG: Changed project name and namespace

--- BlocksHub-GriefPrevention/src/org/PrimeSoft/blocksHub/griefPrevention/BlocksHubPlugin.java
+++ BlocksHub-GriefPrevention/src/org/PrimeSoft/blocksHub/griefPrevention/BlocksHubPlugin.java
@@ -40,7 +40,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.PrimeSoft.blocksHub.griefPrevention8x;
+package org.PrimeSoft.blocksHub.griefPrevention;
 
 import org.PrimeSoft.blocksHub.IBlocksHubApi;
 import java.util.logging.Level;
